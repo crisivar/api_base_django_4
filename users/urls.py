df835921fafa8a084e5ea52020feb736b3dd8a58
Cristian Ivan Rojas Valencia <ingsis3@cenicana.org>
2022-06-21 11:53:46 -0500

    fix: missing /

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('profile', ProfileView.as_view() , name='profile'),
+    path('profile/', ProfileView.as_view() , name='profile'),
 ]
